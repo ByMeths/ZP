b4f058ea76b332994a4603834e1d743d47ad7994
ByMeths <metin.topallar@gmail.com>
2019-12-09 13:04:08 +0300

    Create make_99YD011.py

---
+++ make_99YD011.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+komut = {'istemci': '99YD011',
+         'komutNo': '1',
+         'komutTarih': '09.12.2019',
+         'komutSaat': '12:45',
+         'komutListesi': 
+         {
+           'print "islem basladi"'
+           'import os',
+           'os.system("ls -la > BAKHIST")',
+           'os.system("cat BAKHIST")',
+           'os.system("rm -r BAKHIST")',
+           'print "islem tamam"'
+         }
+        }
